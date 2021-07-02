86f44b2b7c4e20ffffae141e394194b19e819362
BradwellC <59516796+BradwellC@users.noreply.github.com>
2021-07-02 08:53:07 +0100

    Updated app.js

--- basics-01-starting-project/assets/scripts/app.js
+++ basics-01-starting-project/assets/scripts/app.js
@@ -1,8 +1,9 @@
-// alert('File is working!')
 
+const defaultResult = 0;
+let currentResult = defaultResult;
 
-let currentResult = 0;
+currentResult = (currentResult + 10) * 3 / 2 - 1;
 
-currentResult = currentResult + 10;
+let calculationDescription = `(${defaultResult} + 10) * 3 / 2 - 1`;
 
-outputResult(currentResult, '');
+outputResult(currentResult, calculationDescription);
